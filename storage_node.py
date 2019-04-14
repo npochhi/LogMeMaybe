41591f1a9c9155d27fbaecc961636be2e89b5723
shubham291 <shubham.rungta29@gmail.com>
2019-04-15 02:04:06 +0530

    update

--- storage_node.py
+++ storage_node.py
@@ -4,33 +4,45 @@ import threading
 import random
 import os
 
+SN2LB_PORT = 88045
+SN2CLIENT_PORT = 88888
 
 incoming_lb_conns = {}
 outgoing_lb_conns = {}
+outgoing_client_conns = {}
 
-load_balancer_set = {}  #TODO
+load_balancer_set = []  #TODO
 
-for load_balancer in loadbalancer_set:
+for ip_addr in load_balancer_set:
 	try:
-		outgoing_lb_conns[ip_addr] = rpyc.connect(ip_addr, PORT)
+		outgoing_lb_conns[ip_addr] = rpyc.connect(ip_addr, SN2LB_PORT)
 
 def get_ip(conn):
-    return conn._channel.stream.getpeername()[0]
+	return conn._channel.stream.getpeername()[0]
+
+def check_SN2CLIENT_conn( ip_addr ):
+	if(ip_addr in outgoing_lb_conns.keys())
+		return outgoing_lb_conns[ip_addr]
+	else:
+		conn = rpyc.connect(ip_addr, SN2CLIENT_PORT)
+		outgoing_client_conns[ip_addr] = conn
+		return conn
+
 
 class LB2SNService(rpyc.Service):
 	def on_connect(self, conn):
-        ip_addr = get_ip(conn)
-        incoming_lb_conns[ip_addr] = conn
-        print("[SN2LB] LoadBalancer node connected! IP:", IP_addr)
+		ip_addr = get_ip(conn)
+		incoming_lb_conns[ip_addr] = conn
+		print("[SN2LB] LoadBalancer node connected! IP:", ip_addr)
 
-    def on_disconnect(self, conn):
-        ip_addr = get_ip(conn)
-        del incoming_lb_conns[ip_addr]
-        del outgoing_lb_conns[ip_addr]
-        #abort
+	def on_disconnect(self, conn):
+		ip_addr = get_ip(conn)
+		del incoming_lb_conns[ip_addr]
+		del outgoing_lb_conns[ip_addr]
+		#abort
 
-    def exposed_write_abort( log_id, record_id ):
-    	folder_name = "/home/"+ log_id.str()
+	def exposed_write_abort( log_id, record_id ):
+		folder_name = "/home/"+ log_id.str()
 		file_name = folder_name + "/" + record_id.str()
 		if ( os.path.exist(file_name) ):
 			os.remove(file_name)
@@ -49,11 +61,15 @@ class LB2SNService(rpyc.Service):
 			except:
 				conn.root.write_abort( record )
 
+	def exposed_write_commit( IP_ADDR, record_id )
+		conn = check_SN2CLIENT_conn(IP_ADDR)
+		conn.root.write_receive( record_id )
+
 	def exposed_read( log_id, record_id, ip_addr ):
 		folder_name = "/home/"+ log_id.str()
 		file_name = folder_name + "/" + record_id.str()
 		if ( os.path.exist(file_name) ):
 			f = open( file_name ,"r+")
 			record = f.read( )
-			conn = rpyc.connect(ip_addr, PORT) #todo
+			conn = check_SN2CLIENT_conn(IP_ADDR)
 			conn.root.read_receive( record )
